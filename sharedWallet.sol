a7098cf311a5698b69878108b81e81399e8b8d07
Prateek Yadav <113380603+prateek-ydv@users.noreply.github.com>
2024-04-04 12:05:18 +0530

    Update sharedWallet.sol

--- sharedWallet.sol
+++ sharedWallet.sol
@@ -2,21 +2,21 @@
 pragma solidity ^0.8.1;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract sharedWallet is Ownable(msg.sender) {
+contract sharedWallet is Ownable(msg.sender){
     mapping(address =>uint) public allowance;
 
-    
     function addAllowance(address _who, uint _amount) public onlyOwner{
-        allowance[_who] = _amount;
+        allowance[_who] += _amount;
     }
 
     modifier ownerOrAllowed(uint _amount) {
         require(isOwner() || allowance[msg.sender] >= _amount,"you are not allowed");
         _;
     }
-    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
-        
+    function withdrawMoney(address payable _to, uint _amount) public {
+        require(allowance[msg.sender] >= _amount,"you dont have enough balance");
         _to.transfer(_amount);
+        allowance[_to] -=_amount;
     }
     function isOwner() internal view returns(bool) {
         return owner() == msg.sender;
